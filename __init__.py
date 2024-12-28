a8fd576158b22fe4c4a79c506fd22d65b83acfbd
Jeffery Harrell <jefferyharrell@gmail.com>
2024-12-28 11:31:24 -0800

    Add JHPreviewImage to __init__.py

--- __init__.py
+++ __init__.py
@@ -11,11 +11,13 @@ from comfyui_jh_misc_nodes.jh_n_way_switch_node import (
     JHThreeWaySwitchNode,
     JHTwoWaySwitchNode,
 )
+from comfyui_jh_misc_nodes.jh_preview_image import JHPreviewImage
 
 NODE_CLASS_MAPPINGS = {
     "JHDaisyChainableStringConstantNode": JHDaisyChainableStringConstantNode,
     "JHTwoWaySwitchNode": JHTwoWaySwitchNode,
     "JHThreeWaySwitchNode": JHThreeWaySwitchNode,
+    "JHPreviewImage": JHPreviewImage,
 }
 
 
@@ -23,6 +25,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "JHDaisyChainableStringConstantNode": "Daisy-Chainable String Constant",
     "JHTwoWaySwitchNode": "Two-Way Switch",
     "JHThreeWaySwitchNode": "Three-Way Switch",
+    "JHPreviewImage": "Preview Image",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
